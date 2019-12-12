efda38ab25483a38ae5421a55008bc3e44b92913
Philip Craig <philipjcraig@gmail.com>
2019-12-12 13:33:25 +1000

    Always enable 'alloc' when 'std' is enabled
    This allows the cfg attributes to be simpler.

--- src/lib.rs
+++ src/lib.rs
@@ -132,60 +132,41 @@ pub unsafe trait CloneStableDeref: StableDeref + Clone {}
 // std types integration
 /////////////////////////////////////////////////////////////////////////////
 
-#[cfg(all(feature = "std"))]
-use std::boxed::Box;
-#[cfg(all(not(feature = "std"), feature = "alloc"))]
+#[cfg(feature = "alloc")]
 use alloc::boxed::Box;
-
-#[cfg(all(feature = "std"))]
-use std::rc::Rc;
-#[cfg(all(not(feature = "std"), feature = "alloc"))]
+#[cfg(feature = "alloc")]
 use alloc::rc::Rc;
-
-#[cfg(all(feature = "std"))]
-use std::sync::Arc;
-#[cfg(all(not(feature = "std"), feature = "alloc"))]
+#[cfg(feature = "alloc")]
 use alloc::sync::Arc;
-
-#[cfg(all(feature = "std"))]
-use std::vec::Vec;
-#[cfg(all(not(feature = "std"), feature = "alloc"))]
+#[cfg(feature = "alloc")]
 use alloc::vec::Vec;
-
-#[cfg(all(feature = "std"))]
-use std::string::String;
-#[cfg(all(not(feature = "std"), feature = "alloc"))]
+#[cfg(feature = "alloc")]
 use alloc::string::String;
 
 
 #[cfg(feature = "std")]
 use std::sync::{MutexGuard, RwLockReadGuard, RwLockWriteGuard};
 
-#[cfg(feature = "std")]
-use std::cell::{Ref, RefMut};
-#[cfg(not(feature = "std"))]
 use core::cell::{Ref, RefMut};
 
 
-#[cfg(any(feature = "std", feature = "alloc"))]
+#[cfg(feature = "alloc")]
 unsafe impl<T: ?Sized> StableDeref for Box<T> {}
-#[cfg(any(feature = "std", feature = "alloc"))]
+#[cfg(feature = "alloc")]
 unsafe impl<T> StableDeref for Vec<T> {}
-#[cfg(any(feature = "std", feature = "alloc"))]
+#[cfg(feature = "alloc")]
 unsafe impl StableDeref for String {}
 
-#[cfg(any(feature = "std", feature = "alloc"))]
+#[cfg(feature = "alloc")]
 unsafe impl<T: ?Sized> StableDeref for Rc<T> {}
-#[cfg(any(feature = "std", feature = "alloc"))]
+#[cfg(feature = "alloc")]
 unsafe impl<T: ?Sized> CloneStableDeref for Rc<T> {}
-#[cfg(any(feature = "std", feature = "alloc"))]
+#[cfg(feature = "alloc")]
 unsafe impl<T: ?Sized> StableDeref for Arc<T> {}
-#[cfg(any(feature = "std", feature = "alloc"))]
+#[cfg(feature = "alloc")]
 unsafe impl<T: ?Sized> CloneStableDeref for Arc<T> {}
 
-// #[cfg(any(feature = "std", feature = "alloc"))]
 unsafe impl<'a, T: ?Sized> StableDeref for Ref<'a, T> {}
-// #[cfg(any(feature = "std", feature = "alloc"))]
 unsafe impl<'a, T: ?Sized> StableDeref for RefMut<'a, T> {}
 #[cfg(feature = "std")]
 unsafe impl<'a, T: ?Sized> StableDeref for MutexGuard<'a, T> {}
